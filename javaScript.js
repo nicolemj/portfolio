11183fe79d255a7c9c1070f5b083802ea91c52f2
Nicole jeffries <nicolejeffries0118@gmail.com>
2018-01-25 08:22:23 -0500

    Added resume

--- javaScript.js
+++ javaScript.js
@@ -5,3 +5,9 @@ $(document).ready(function() {
   })
 });
 
+$('a[rel=popover]').popover({
+  html: true,
+  trigger: 'hover',
+  placement: 'bottom',
+  content: function(){return '<img src="'+$(this).data('img') + '" />';}
+});
